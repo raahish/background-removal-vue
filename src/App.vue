45bb59509e8d0fa7554d2c789149c954508c827d
Alon Burg <burgalon@gmail.com>
2017-06-10 10:39:07 +0300

    dropzone, look and feel + 2class tiramisu

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <template>
   <div id="app">
+    <div class="container">
+      <div class="text-center">
+        <a href="/" title="GreenScreen - Cut your images with AI">
+          <img src="./assets/logo-with-text.svg" width="50%" alt="GreenScreen - Cut your images with AI "/>
+        </a>
+      </div>
+    </div>
+    <div class="navbar navbar-inverse">
+      <div class="container">
+        <ul class="nav navbar-nav">
+          <li class="navbar-text">using Keras.JS</li>
+          <li><a class="navbar-link" href="https://medium.com/p/35648f9dc5fb">Blog Post</a></li>
+          <li><a class="navbar-link" href="https://greenscreen-ai.boorgle.com/">Server-side solution</a></li>
+        </ul>
+      </div>
+    </div>
     <router-view></router-view>
   </div>
 </template>
@@ -11,12 +27,28 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url('//fonts.googleapis.com/css?family=Open+Sans:400,700,800|Open+Sans+Condensed:300,700');
+  @import url('//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css');
+
+  body {
+    font-family: "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+
+  #app {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+  }
+  #content {
+    flex: 1;
+  }
+
+  .container {
+    max-width: 960px;
+  }
+
+
 </style>
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,40 +1,40 @@
 
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <div class="loading-progress" v-if="modelLoading && loadingProgress < 100">
-      Loading...{{ loadingProgress }}%
+  <div id="content" class="container">
+    <div class="row">
+      <div class="col-md-push-2 col-md-8 col-xs-12">
+
+      </div>
+    </div>
+    <div class="text-center loading-progress" v-if="modelLoading">
+      Loading Keras model...{{ loadingProgress }}%
     </div>
 
-    <h2>Enter image URL</h2>
-    <input
-      v-model="imageURLInput"
-      spellcheck="false"
-      @change="onImageURLInputEnter"
-      @keyup.enter="onImageURLInputEnter"
-    >
+    <dropzone
+      :class="{hidden: modelLoading}"
+      ref="dropzone"
+      id="dropzone"
+      url="#"
+      :dropzoneOptions="{resizeWidth: 448, url: '#', parallelUploads: 1}"
+      :useCustomDropzoneOptions="true"></dropzone>
+
     <div class="error" v-if="imageLoadingError">Error loading URL</div>
-    <div class="canvas-container">
-      <canvas id="input-canvas" width="224" height="224"></canvas>
-      <canvas id="output-canvas" width="224" height="224"></canvas>
+    <div class="results" v-for="src of output">
+      <img :src="src" />
     </div>
-    <div v-if="imageLoading || modelRunning">Calculating</div>
   </div>
 </template>
+
 
 <script lang="babel">
 
 const MODEL_FILEPATHS_DEV = {
-  model: 'static/model/model.json',
-  weights: 'static/model/model_weights.buf',
-  metadata: 'static/model/model_metadata.json'
+  model: 'static/model/tiramisu_2_classes.json',
+  weights: 'static/model/tiramisu_2_classes_weights.buf',
+  metadata: 'static/model/tiramisu_2_classes_metadata.json'
 }
-//const MODEL_FILEPATHS_PROD = {
-//  model: 'https://transcranial.github.io/keras-js-demos-data/mnist_cnn/mnist_cnn.json',
-//  weights: 'https://transcranial.github.io/keras-js-demos-data/mnist_cnn/mnist_cnn_weights.buf',
-//  metadata: 'https://transcranial.github.io/keras-js-demos-data/mnist_cnn/mnist_cnn_metadata.json'
-//}
-const MODEL_CONFIG = { filepaths: MODEL_FILEPATHS_DEV, gpu: true }
+const BACKGROUND_CLASS = 0
+const MODEL_CONFIG = { filepaths: MODEL_FILEPATHS_DEV, gpu: true, layerCallPauses: true }
 
 const COLOR_CODES = [
   [128, 128, 128],
@@ -53,10 +53,12 @@ const COLOR_CODES = [
 const NAMES = ['sky', 'building', 'column_pole', 'road', 'sidewalk', 'tree',
   'sign', 'fence', 'car', 'pedestrian', 'bicyclist', 'void']
 import * as KerasJS from 'keras-js'
+import _ from 'lodash'
 import loadImage from 'blueimp-load-image'
 import ndarray from 'ndarray'
 import ops from 'ndarray-ops'
 import * as utils from '../utils'
+import Dropzone from 'vue2-dropzone'
 
 window.ndarray = ndarray
 window.ops = ops
@@ -64,71 +66,85 @@ window.utils = utils
 
 export default {
   name: 'hello',
+  components: {
+    Dropzone
+  },
   data () {
     return {
       model: new KerasJS.Model(MODEL_CONFIG), // eslint-disable-line
       modelLoading: true,
-      imageURLInput: 'static/container.jpg',
       imageURLSelect: null,
       imageLoading: false,
       imageLoadingError: false,
       modelRunning: false,
-      output: null,
-      msg: 'Welcome to Your Vue.js App'
+      output: [],
+      files: [],
+      intervals: [],
     }
   },
   computed: {
     loadingProgress: function () {
       return this.model.getLoadingProgress()
-    }
+    },
   },
   mounted: function () {
+    // https://stackoverflow.com/questions/33710825/getting-file-contents-when-using-dropzonejs
+//    this.$refs.dropzone.dropzone.accept = this.localAcceptHandler
+    this.$refs.dropzone.dropzone.uploadFiles = this.uploadFiles
     this.model.ready().then(() => {
       this.modelLoading = false
-      this.$nextTick(() => {
-//        this.loadImageToCanvas(this.imageURLInput)
-      })
     })
   },
   methods: {
-    onImageURLInputEnter: function(e) {
-      this.loadImageToCanvas(e.target.value)
+    readLocalFile: function(file) {
+      return new Promise((resolve,reject) => {
+        let reader = new window.FileReader();
+        reader.onload = () => resolve(reader.result)
+        reader.onerror = () => reject(reader.result)
+        // run the reader
+        reader.readAsDataURL(file);
+      });
     },
-    loadImageToCanvas: function(url) {
-      if (!url) {
-        this.clearAll()
-        return
-      }
+    loadNextImageToCanvas: function() {
+      let file = this.files.pop()
+      this.runModel(file)
+    },
 
-      this.imageLoading = true
-      loadImage(
-        'https://crossorigin.me/' + url,
-        img => {
-          if (img.type === 'error') {
-            this.imageLoadingError = true
-            this.imageLoading = false
-          } else {
-            // load image data onto input canvas
-            const ctx = document.getElementById('input-canvas').getContext('2d')
-            ctx.drawImage(img, 0, 0)
-            this.imageLoadingError = false
-            this.imageLoading = false
-            this.modelRunning = true
-            // model predict
-            this.$nextTick(function() {
-              setTimeout(() => {
-                this.runModel()
-              }, 200)
-            })
-          }
-        },
-        { maxWidth: 224, maxHeight: 224, cover: true, crop: true, canvas: true, crossOrigin: 'Anonymous' }
-      )
+    updateDropzoneFileProgress: function(file, progress) {
+      const self = this.$refs.dropzone.dropzone
+      self.emit('uploadprogress', file, progress)
+      if(progress == 100) {
+        file.status = Dropzone.SUCCESS;
+        self.emit("success", file, 'success', null);
+        self.emit("complete", file);
+        self.processQueue();
+      }
     },
-    runModel: function() {
-      const ctx = document.getElementById('input-canvas').getContext('2d')
-      const imageData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height)
-      const { data, width, height } = imageData
+
+    uploadFiles: function(files) {
+      files.forEach(file => {
+        this.readLocalFile(file).then(result => {
+          const img =  new Image
+          img.width = 224
+          img.height = 224
+          img.src = result
+          return this.runModel(img, file)
+        })
+      })
+    },
+
+    beforeDestroy: function() {
+      this.intervals.forEach(i => clearInterval(i))
+    },
+
+    runModel: function(img, file) {
+      let canvas = document.createElement('canvas')
+      canvas.width = 224
+      canvas.height = 224
+      const ctx = canvas.getContext('2d')
+      ctx.drawImage(img, 0, 0, img.width, img.height)
+      const { data, width, height } = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height)
+      window.imageData = data
 
 //      // data processing
 //      // see https://github.com/fchollet/keras/blob/master/keras/applications/imagenet_utils.py
@@ -138,48 +154,64 @@ export default {
       ops.assign(dataProcessedTensor.pick(null, null, 0), dataTensor.pick(null, null, 0))
       ops.assign(dataProcessedTensor.pick(null, null, 1), dataTensor.pick(null, null, 1))
       ops.assign(dataProcessedTensor.pick(null, null, 2), dataTensor.pick(null, null, 2))
-      const inputData = { input_9: dataProcessedTensor.data }
-      console.log("start")
-      this.model.predict(inputData).then(outputData => {
+      console.log('inputTensors', this.model.inputTensors)
+      const inputKey = Object.keys(this.model.inputTensors)[0]
+      const inputData = { [inputKey]: dataProcessedTensor.data }
+      const n_layers = Object.keys(this.model.modelDAG).length
+      console.log("start", n_layers)
+      const intervalId = setInterval(() => {
+        console.log(this.model.layersWithResults.length)
+        this.updateDropzoneFileProgress(file, this.model.layersWithResults.length/n_layers*90)
+      }, 1000)
+      return this.model.predict(inputData).then(outputData => {
         console.log("done")
-        this.modelRunning = false
-        window.outputData = outputData = outputData.activation_614
-        let argmaxArray = new Uint8ClampedArray(width*height*4)
+        clearInterval(intervalId)
+        this.updateDropzoneFileProgress(file, 100)
+        let outputLayerName = _.values(this.model.modelDAG).filter(node => !node.outbound.length)[0].name
+        window.outputData = outputData = outputData[outputLayerName]
+        console.log('outputLayerName', outputLayerName)
+        let n_activations = outputData.length / width / height
+        console.log('n_activations', n_activations)
+        const argmaxArray = new Uint8ClampedArray(width*height*4) // 4 channels RGBA
         let ii=0;
         for(let xy=0; xy<width*height; xy++) {
           let max = 0
           let maxValue = 0
-          for(let i=0; i<12; i++) {
+          for(let i=0; i<n_activations; i++) {
             if(maxValue<outputData[ii]) {
               max = i
+              maxValue = outputData[ii]
             }
             ii+=1
           }
-          argmaxArray[xy*4] = COLOR_CODES[max][0]
-          argmaxArray[xy*4+1] = COLOR_CODES[max][1]
-          argmaxArray[xy*4+2] = COLOR_CODES[max][2]
-          argmaxArray[xy*4+3] = 255
+//          argmaxArray[xy*4] = COLOR_CODES[max][0]
+//          argmaxArray[xy*4+1] = COLOR_CODES[max][1]
+//          argmaxArray[xy*4+2] = COLOR_CODES[max][2]
+//          argmaxArray[xy*4+3] = 255
+
+          argmaxArray[xy*4] = data[xy*4]
+          argmaxArray[xy*4+1] = data[xy*4+1]
+          argmaxArray[xy*4+2] = data[xy*4+2]
+          argmaxArray[xy*4+3] = max!=BACKGROUND_CLASS ? maxValue*255 : 0
         }
-        window.output = this.output = new ImageData(argmaxArray, width, height)
-        this.drawOutput()
+        this.drawOutput(new ImageData(argmaxArray, width, height))
+        if(this.files.length) {
+          this.loadNextImageToCanvas()
+        } else {
+          this.modelRunning = false
+        }
+        console.log('done displaying')
       })
     },
-    drawOutput: function() {
-      const ctx = document.getElementById('output-canvas').getContext('2d')
-      ctx.putImageData(this.output, 0, 0)
+    drawOutput: function(imageData) {
+      const canvas = document.createElement('canvas')
+      canvas.width = 224
+      canvas.height = 224
+      const ctx = canvas.getContext('2d')
+      ctx.putImageData(imageData, 0, 0)
+
+      this.output.push(canvas.toDataURL('image/png'))
     },
-    clearAll: function() {
-      this.modelRunning = false
-      this.imageURLInput = null
-      this.imageLoading = false
-      this.imageLoadingError = false
-      this.output = null
-
-      this.model.layersWithResults = []
-
-      const ctx = document.getElementById('input-canvas').getContext('2d')
-      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-    }
   }
 
 }
@@ -188,6 +220,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+@import url('~dropzone/dist/dropzone.css');
 h1, h2 {
   font-weight: normal;
 }
@@ -205,4 +238,36 @@ li {
 a {
   color: #42b983;
 }
+
+.alert-danger {
+  color: #fff;
+  background-color: #ff3f49;
+  margin: 30px 0;
+}
+
+.results img {
+  max-width: 100%;
+  max-height: 500px;
+  display: block;
+  margin: 30px auto;
+}
+
+.dropzone {
+  border: 2px dashed #0087F7;
+  border-radius: 5px;
+  background: white;
+}
+
+.dropzone .dz-message {
+  font-weight: 400;
+}
+
+.dropzone .dz-message .note {
+  font-size: 0.8em;
+  font-weight: 200;
+  display: block;
+  margin-top: 1.4rem;
+}
+
+
 </style>
